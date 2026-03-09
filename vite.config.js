2ba3b0d04482889477ae48db62a383bce4915250
Dharmesh-Makvana <edgeroboticsstudio@gmail.com>
2026-03-09 11:57:00 +0530

    simplify Vite base path configuration to always be root.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "/edge-robotics-studio/" : "/",
+  base: "/",
   build: {
     rollupOptions: {
       output: {
